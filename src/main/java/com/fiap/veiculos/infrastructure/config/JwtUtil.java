a6727f3dddf410889aa9124b393317e7891dac38
Ana Julia <anajulialadislau18@gmail.com>
2024-10-24 23:11:37 -0300

    refresh de todos os arquivos

--- src/main/java/com/fiap/veiculos/infrastructure/config/JwtUtil.java
+++ src/main/java/com/fiap/veiculos/infrastructure/config/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.fiap.veiculos.infrastructure.config;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
--- src/main/java/com/fiap/veiculos/infrastructure/persistence/repository/VeiculoRepository.java
+++ src/main/java/com/fiap/veiculos/infrastructure/persistence/repository/VeiculoRepository.java
@@ -10,6 +10,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;

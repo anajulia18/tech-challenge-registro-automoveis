d26e15697c79aaa4d3310d77d0dd0c48f2708c79
Ana Julia <anajulialadislau18@gmail.com>
2024-10-25 23:18:08 -0300

    atualizacao auth

---
+++ src/main/java/com/fiap/veiculos/infrastructure/config/JwtUtil.java
@@ -0,0 +1,29 @@
+package com.fiap.veiculos.infrastructure.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+
+    public static final String SECRET = "fiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiapfiap";
+
+
+    public void validateToken(final String token) {
+        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+
+    }
+
+
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
--- src/main/java/com/fiap/veiculos/infrastructure/persistence/repository/VendaRepository.java
+++ src/main/java/com/fiap/veiculos/infrastructure/persistence/repository/VendaRepository.java
@@ -14,6 +14,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Component
